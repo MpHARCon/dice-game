cf9486d6ad22f38f1437cf008a6e6598879ce75a
boldoo <boldoo.temka@gmail.com>
2024-01-16 21:02:18 +0800

    Shoo shideh ued zurag ni delgets deer haragdah

---
+++ app.js
@@ -0,0 +1,19 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэнэ
+var ActivePlayer = 1;
+// Тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+// Шоны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+var dice = Math.floor(Math.random() * 6) + 1;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
